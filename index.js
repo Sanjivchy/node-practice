cf05473001ac367224814b05381db65ae64d039c
sanjivchaudhary416 <sanjivchaudhary416@gmail.com>
2024-01-27 18:29:31 +0000

    express server setup

--- index.js
+++ index.js
@@ -8,14 +8,29 @@ app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('^/$|/index|/index.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+app.get('/api/user', (req, res) => {
+  res.json(
+    {
+      name: 'John',
+      age: 30
+    }
+  );
+});
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
 
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect('/new-page.html');
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
